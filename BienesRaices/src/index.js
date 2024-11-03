cd720758c6adc88eef9b4b48147d207e14e3dde9
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-11-03 16:27:06 -0300

    config: Importamos e instanciamos la configuracion para la conexion a la db desde el index principal con un manejo de errores

--- BienesRaices/src/index.js
+++ BienesRaices/src/index.js
@@ -1,10 +1,19 @@
 // const express   = require('express'); 
 import express from 'express'
 import usuarioRouter from './routers/usuarioRouter.js'
-
+import db from './config/db.js'
 
 //Crear la app
 const app = express();
+
+//Conexion a la base de datos
+try {
+    await db.authenticate();
+    console.log('Conexion correcta a la base de datos!!..')
+
+} catch (error) {
+    console.log(`Ha ocurrido un error ${error}`)
+}
 
 //Routing
 //La funcion .use se utiliza para escanear todas las rutas actuando como middlewares
