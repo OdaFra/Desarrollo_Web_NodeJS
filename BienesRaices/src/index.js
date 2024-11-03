0983aecb91ad3f075b75efe53c05b44ed6ab3054
Oscar Ramirez <oscar.ramirez@millicom.com>
2024-11-03 16:54:29 -0300

    refactor: Agregamos sync para manejo de creacion de tabla y aplicamos instancia de process.env para las variables de entorno

--- BienesRaices/src/index.js
+++ BienesRaices/src/index.js
@@ -1,40 +1,37 @@
-// const express   = require('express'); 
-import express from 'express'
-import usuarioRouter from './routers/usuarioRouter.js'
-import db from './config/db.js'
+// const express   = require('express');
+import express from "express";
+import usuarioRouter from "./routers/usuarioRouter.js";
+import db from "./config/db.js";
 
 //Crear la app
 const app = express();
 
+// Habilitar lectura  de datos del formulario
+app.use(express.urlencoded({ extended: true }));
+
 //Conexion a la base de datos
 try {
-    await db.authenticate();
-    console.log('Conexion correcta a la base de datos!!..')
-
+  await db.authenticate();
+  db.sync()
+  console.log("Conexion correcta a la base de datos!!..");
 } catch (error) {
-    console.log(`Ha ocurrido un error ${error}`)
+  console.log(`Ha ocurrido un error ${error}`);
 }
 
 //Routing
 //La funcion .use se utiliza para escanear todas las rutas actuando como middlewares
-app.use('/auth', usuarioRouter)
-
+app.use("/auth", usuarioRouter);
 
 //Carpeta publicas
-app.use(express.static('./src/public'))
-
+app.use(express.static("./src/public"));
 
 //Habilitar Pug
-app.set('view engine', 'pug')
-app.set('views', './src/views')
-
-
-
-
+app.set("view engine", "pug");
+app.set("views", "./src/views");
 
 //Definir un puerto para iniciar
-const PORT = 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, ()=>{
-    console.log(`El servidor ha iniciado en el puerto ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`El servidor ha iniciado en el puerto ${PORT}`);
+});
